01cab93ba7d02094862af75b2343f036f586e230
Harry Chen <harry@harry.gg>
2015-09-04 15:24:09 +0800

    feat(intents): Include expressions while listing intents

--- app/controllers/api/v1/intents_controller.rb
+++ app/controllers/api/v1/intents_controller.rb
@@ -4,20 +4,37 @@ module Api
       def index
         access_token = ENV['WIT_SERVER_ACCESS_TOKEN']
 
-        render json: list_all_intent(access_token)
+        render json: list_all_intents(access_token)
+      end
+
+      def show
+        access_token = ENV['WIT_SERVER_ACCESS_TOKEN']
+        intent_wit_id = params[:id]
+
+        render json: get_single_intent(access_token, intent_wit_id)
       end
 
       private
 
-      def list_all_intent(access_token)
-        require 'open-uri'
+        def list_all_intents(access_token)
+          require 'open-uri'
 
-        url = "https://api.wit.ai/intents"
-        query = "?"
-        query += "access_token=#{access_token}"
+          url = "https://api.wit.ai/intents"
+          query = "?"
+          query += "access_token=#{access_token}"
 
-        url_data = open(url+query).read
-      end
+          url_data = open(url+query).read
+        end
+
+        def get_single_intent(access_token, intent_wit_id)
+          require 'open-uri'
+
+          url = "https://api.wit.ai/intents/#{intent_wit_id}"
+          query = "?"
+          query += "access_token=#{access_token}"
+
+          url_data = open(url+query).read
+        end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'ask' => 'chats#ask'
-      resources :intents, only: [:index]
+      resources :intents, only: [:index, :show]
     end
   end
 end
