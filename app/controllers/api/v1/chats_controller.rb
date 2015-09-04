95a03aaeb510f39907d7d41e12118795d578c048
Harry Chen <harry@harry.gg>
2015-09-04 10:13:50 +0800

    feat(api): Change API folder structure

---
+++ app/controllers/api/v1/chats_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class ChatsController < ApplicationController
+      def ask
+        require 'json'
+
+        access_token = ENV['WIT_SERVER_ACCESS_TOKEN']
+
+        user_query = params[:message]
+
+        response = get_intent user_query, access_token
+        outcomes = JSON.parse(response)["outcomes"][0]
+        intent = outcomes["intent"]
+        confidence = outcomes["confidence"]
+
+        response = Response.where(intent: intent).first.response
+
+        render json: {
+          user_query: user_query,
+          intent: intent,
+          confidence: confidence,
+          response: response
+        }, status: 200
+      end
+
+      private
+
+      def get_intent(message, access_token)
+        require 'open-uri'
+
+        url = "https://api.wit.ai/message?access_token=#{access_token}&q=#{message}"
+
+        url_data = open(url).read
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get 'ask' => 'chats#ask'
+  namespace :api do
+    namespace :v1 do
+      get 'ask' => 'chats#ask'
+    end
+  end
 end
